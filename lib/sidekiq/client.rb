e7ae72d3d3ee747a9c5d5b3c4fa147869263f194
Mike Perham <mperham@gmail.com>
2012-01-23 12:56:49 -0800

    More client testing

--- lib/sidekiq/client.rb
+++ lib/sidekiq/client.rb
@@ -13,12 +13,12 @@ module Sidekiq
     end
 
     # Example usage:
-    # Sidekiq::Client.push('my_queue', :class => MyWorker, :args => ['foo', 1, :bat => 'bar'])
+    # Sidekiq::Client.push('my_queue', 'class' => MyWorker, 'args' => ['foo', 1, :bat => 'bar'])
     def self.push(queue, item)
       raise(ArgumentError, "Message must be a Hash of the form: { :class => SomeClass, :args => ['bob', 1, :foo => 'bar'] }") unless item.is_a?(Hash)
-      raise(ArgumentError, "Message must include a class and set of arguments: #{item.inspect}") if !item[:class] || !item[:args]
+      raise(ArgumentError, "Message must include a class and set of arguments: #{item.inspect}") if !item['class'] || !item['args']
 
-      item[:class] = item[:class].to_s if !item[:class].is_a?(String)
+      item['class'] = item['class'].to_s if !item['class'].is_a?(String)
       redis.rpush("queue:#{queue}", MultiJson.encode(item))
     end
 
@@ -35,7 +35,7 @@ module Sidekiq
     #   end
     #
     def self.enqueue(klass, *args)
-      queue = klass.instance_variable_get(:@queue) || (klass.respond_to?(:queue) && klass.queue) || raise(ArgumentError, "Cannot determine queue to use")
+      queue = (klass.respond_to?(:queue) && klass.queue) || klass.instance_variable_get(:@queue) || raise(ArgumentError, "Cannot determine queue to use")
       push(queue, { 'class' => klass.name, 'args' => args })
     end
   end
--- test/test_client.rb
+++ test/test_client.rb
@@ -2,16 +2,40 @@ require 'helper'
 require 'sidekiq/client'
 
 class TestClient < MiniTest::Unit::TestCase
-  def test_argument_handling
-    assert_raises ArgumentError do
-      Sidekiq::Client.push('foo', 1)
+  describe 'with mock redis' do
+    before do
+      @redis = MiniTest::Mock.new
+      Sidekiq::Client.redis = @redis
     end
 
-    assert_raises ArgumentError do
-      Sidekiq::Client.push('foo', :class => 'Foo', :noargs => [1, 2])
+    it 'raises ArgumentError with invalid params' do
+      assert_raises ArgumentError do
+        Sidekiq::Client.push('foo', 1)
+      end
+
+      assert_raises ArgumentError do
+        Sidekiq::Client.push('foo', :class => 'Foo', :noargs => [1, 2])
+      end
     end
 
-    count = Sidekiq::Client.push('foo', :class => 'Foo', :args => [1, 2])
-    assert count > 0
+    it 'pushes messages to redis' do
+      @redis.expect :rpush, 1, ['queue:foo', String]
+      count = Sidekiq::Client.push('foo', 'class' => 'Foo', 'args' => [1, 2])
+      assert count > 0
+      @redis.verify
+    end
+
+    class MyWorker
+      def self.queue
+        'foo'
+      end
+    end
+
+    it 'enqueues messages to redis' do
+      @redis.expect :rpush, 1, ['queue:foo', String]
+      count = Sidekiq::Client.enqueue(MyWorker, 1, 2)
+      assert count > 0
+      @redis.verify
+    end
   end
 end
