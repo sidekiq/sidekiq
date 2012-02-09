2399d13dbbccda1f54e27ee5bbdec9b71dcc5868
Ryan LeCompte <lecompte@gmail.com>
2012-02-08 17:28:12 -0800

    add tests for non-yielding middleware

--- test/test_middleware.rb
+++ test/test_middleware.rb
@@ -38,6 +38,11 @@ class TestMiddleware < MiniTest::Unit::TestCase
       end
     end
 
+    class NonYieldingMiddleware
+      def call(*args)
+      end
+    end
+
     it 'executes middleware in the proper order' do
       Sidekiq::Middleware::Server::UniqueJobs.class_eval do
         def call(worker, msg); yield; end
@@ -55,9 +60,20 @@ class TestMiddleware < MiniTest::Unit::TestCase
       processor.process(msg)
       assert_equal recorder.flatten, %w(0 before 1 before work_performed 1 after 0 after)
     end
+
+    it 'allows middleware to abruptly stop processing rest of chain' do
+      recorder = []
+      chain = Sidekiq::Middleware::Chain.new
+
+      chain.register do
+        use NonYieldingMiddleware
+        use CustomMiddleware, 1, recorder
+      end
+
+      final_action = nil
+      chain.invoke { final_action = true }
+      assert_equal final_action, nil
+      assert_equal recorder, []
+    end
   end
 end
-
-
-
-
