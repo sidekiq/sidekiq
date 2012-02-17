4fedfdf049c3dbc14e51274096b76b860f29d05c
Mike Perham <mperham@gmail.com>
2012-02-17 15:54:11 -0800

    I'm just going to leave this here.  No tests, beware.

---
+++ lib/sidekiq/extensions/active_record.rb
@@ -0,0 +1,29 @@
+require 'sidekiq/extensions/generic_proxy'
+
+module Sidekiq
+  module Extensions
+    ##
+    # Adds a 'delay' method to ActiveRecord to offload arbitrary method
+    # execution to Sidekiq.  Examples:
+    #
+    # User.delay.delete_inactive
+    # User.recent_signups.each { |user| user.delay.mark_as_awesome }
+    class DelayedModel
+      include Sidekiq::Worker
+
+      def perform(yml)
+        (target, method_name, args) = YAML.load(yml)
+        target.send(method_name, *args)
+      end
+    end
+
+    module ActiveRecord
+      def delay
+        Proxy.new(DelayedModel, self)
+      end
+    end
+
+    ::ActiveRecord::Base.extend(ActiveRecord)
+    ::ActiveRecord::Base.include(ActiveRecord)
+  end
+end if defined?(::ActiveRecord)
---
+++ lib/sidekiq/extensions/generic_proxy.rb
@@ -0,0 +1,21 @@
+module Sidekiq
+  module Extensions
+    class Proxy < BasicObject
+      def initialize(performable, target)
+        @performable = performable
+        @target = target
+      end
+
+      def method_missing(name, *args)
+        # Sidekiq has a limitation in that its message must be JSON.
+        # JSON can't round trip real Ruby objects so we use YAML to
+        # serialize the objects to a String.  The YAML will be converted
+        # to JSON and then deserialized on the other side back into a
+        # Ruby object.
+        obj = [@target, name, args]
+        Sidekiq::Client.push('class' => @performable.name, 'args' => [YAML.dump(obj)])
+      end
+    end
+
+  end
+end
