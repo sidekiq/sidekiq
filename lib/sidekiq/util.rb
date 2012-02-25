bf377d44e868d065a78f660e8dc4094a32f17380
Mike Perham <mperham@gmail.com>
2012-02-25 13:43:53 -0800

    Better logging, fixes GH-55

--- lib/sidekiq/util.rb
+++ lib/sidekiq/util.rb
@@ -6,10 +6,18 @@ module Sidekiq
   #
   module Util
 
+    class Pretty < Logger::Formatter
+      # Provide a call() method that returns the formatted message.
+      def call(severity, time, program_name, message)
+        "#{time.utc.iso8601} #{Process.pid} TID-#{Thread.current.object_id.to_s(36)} #{severity}: #{message}\n"
+      end
+    end
+
     def self.logger
       @logger ||= begin
-        log = Logger.new(STDERR)
+        log = Logger.new(STDOUT)
         log.level = Logger::INFO
+        log.formatter = Pretty.new
         log
       end
     end
