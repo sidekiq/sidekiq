20e51f12763f56600ae717f521919843da58ca70
Mike Perham <mperham@gmail.com>
2012-03-05 19:27:27 -0800

    Move all web stuff into /web, fix trailing slash issue.

--- lib/sidekiq/web.rb
+++ lib/sidekiq/web.rb
@@ -18,6 +18,10 @@ module Sidekiq
     end
 
     def call(env)
+      # Solve the problem of people requesting /sidekiq when they need to request /sidekiq/ so
+      # that relative links in templates resolve correctly.
+      return [301, { 'Location' => "#{env['SCRIPT_NAME']}/" }, []] if env['SCRIPT_NAME'] == env['REQUEST_PATH']
+
       return @app.call(env) unless @matcher =~ env["PATH_INFO"]
       env['PATH_INFO'].sub!(@matcher,'')
       @environment.call(env)
@@ -25,10 +29,10 @@ module Sidekiq
   end
 
   class Web < Sinatra::Base
-    dir = File.expand_path(File.dirname(__FILE__) + "/../..")
+    dir = File.expand_path(File.dirname(__FILE__) + "/../../web")
     set :views,  "#{dir}/views"
     set :root, "#{dir}/public"
-    use SprocketsMiddleware, :root => dir, :path => 'assets'
+    use SprocketsMiddleware, :root => dir
 
     get "/" do
       slim :index
