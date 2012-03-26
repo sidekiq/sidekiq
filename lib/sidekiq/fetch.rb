a21729611aee8fd27fd0b6434259453c2f8f53a0
Mike Perham <mperham@gmail.com>
2012-03-25 19:52:15 -0700

    Simpler fetch logic/design

--- lib/sidekiq/fetch.rb
+++ lib/sidekiq/fetch.rb
@@ -10,27 +10,34 @@ module Sidekiq
     include Celluloid
     include Sidekiq::Util
 
+    TIMEOUT = 1
+
     def initialize(mgr, queues)
       @cmd = queues.map { |q| "queue:#{q}" }
       @mgr = mgr
 
       # One second timeout for blpop.
-      # We can't block forever or else we can't shut down
-      # properly.
-      @cmd << 1
+      @cmd << TIMEOUT
     end
 
+    # Fetching is straightforward: the Manager makes a fetch
+    # request for each idle processor when Sidekiq starts and
+    # then issues a new fetch request every time a Processor
+    # finishes a message.
+    #
+    # Because we have to shut down cleanly, we can't block
+    # forever and we can't loop forever.  Instead we reschedule
+    # a new fetch if the current fetch turned up nothing.
     def fetch
       watchdog('Fetcher#fetch died') do
 
         msg = nil
         Sidekiq.redis do |conn|
-          a = Time.now
           (queue, msg) = conn.blpop *@cmd
-          p [Time.now - a, queue, msg]
+          puts 'All quiet' unless msg
           @mgr.assign! msg, queue.gsub(/\Aqueue:/, '') if msg
         end
-        after(0) { fetch } unless msg
+        after(0) { fetch } if !msg
 
       end
     end
