6f01e26c6024ca07bde719e95066bd9fa79637d0
Alexey <leopard.not.a@gmail.com>
2012-04-15 16:46:27 +0300

    not starting with pty = true

--- lib/sidekiq/capistrano.rb
+++ lib/sidekiq/capistrano.rb
@@ -20,8 +20,7 @@ Capistrano::Configuration.instance.load do
     desc "Start sidekiq"
     task :start, :roles => lambda { fetch(:sidekiq_role) } do
       rails_env = fetch(:rails_env, "production")
-      run "cd #{current_path} ; nohup bundle exec sidekiq -e #{rails_env} -C #{current_path}/config/sidekiq.yml -P #{current_path}/tmp/pids/sidekiq.pid >> #{current_path}/log/sidekiq.log 2>&1 &"
-    end
+      run "cd #{current_path} ; nohup bundle exec sidekiq -e #{rails_env} -C #{current_path}/config/sidekiq.yml -P #{current_path}/tmp/pids/sidekiq.pid >> #{current_path}/log/sidekiq.log 2>&1 &", :pty => false
 
     desc "Restart sidekiq"
     task :restart, :roles => lambda { fetch(:sidekiq_role) } do
