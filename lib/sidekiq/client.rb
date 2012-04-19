4902d60134428059ed58887cce23cff43d653f97
Mike Perham <mperham@gmail.com>
2012-04-19 14:42:17 -0700

    Better client rdoc, remove deprecated api

--- lib/sidekiq/client.rb
+++ lib/sidekiq/client.rb
@@ -19,20 +19,27 @@ module Sidekiq
       Sidekiq.redis { |x| x.smembers('queues') }
     end
 
-    # DEPRECATED
-    def self.queue_mappings
-      @queue_mappings ||= {}
-    end
-
-    # Example usage:
-    # Sidekiq::Client.push('queue' => 'my_queue', 'class' => MyWorker, 'args' => ['foo', 1, :bat => 'bar'])
+    ##
+    # The main method used to push a job to Redis.  Accepts a number of options:
+    #
+    #   queue - the named queue to use, default 'default'
+    #   class - the worker class to call, required
+    #   args - an array of simple arguments to the perform method, must be JSON-serializable
+    #   retry - whether to retry this job if it fails, true or false, default true
+    #
+    # All options must be strings, not symbols.  NB: because we are serializing to JSON, all
+    # symbols in 'args' will be converted to strings.
+    #
+    # Example:
+    #   Sidekiq::Client.push('queue' => 'my_queue', 'class' => MyWorker, 'args' => ['foo', 1, :bat => 'bar'])
+    #
     def self.push(item)
       raise(ArgumentError, "Message must be a Hash of the form: { 'class' => SomeWorker, 'args' => ['bob', 1, :foo => 'bar'] }") unless item.is_a?(Hash)
       raise(ArgumentError, "Message must include a class and set of arguments: #{item.inspect}") if !item['class'] || !item['args']
       raise(ArgumentError, "Message must include a Sidekiq::Worker class, not class name: #{item['class'].ancestors.inspect}") if !item['class'].is_a?(Class) || !item['class'].respond_to?('get_sidekiq_options')
 
       item['retry'] = !!item['class'].get_sidekiq_options['retry']
-      queue = item['queue'] || item['class'].get_sidekiq_options['queue'] || queue_mappings[item['class'].to_s] || 'default'
+      queue = item['queue'] || item['class'].get_sidekiq_options['queue'] || 'default'
       worker_class = item['class']
       item['class'] = item['class'].to_s
 
