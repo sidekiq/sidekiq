c4e6fae37c4caf9ab4907af1c0d5e45f324e7d90
Pan Thomakos <pan.thomakos@gmail.com>
2012-05-01 15:54:04 -0700

    Added inline testing functionality.
    Allows sidekiq jobs to run inline in test mode without having to modify
    the production code. In other words production code can still call the
    `perform_async` method and tests can still verify that functionality is
    run inline.

---
+++ test/test_testing_inline.rb
@@ -0,0 +1,75 @@
+require 'helper'
+require 'sidekiq'
+require 'sidekiq/worker'
+require 'active_record'
+require 'action_mailer'
+require 'sidekiq/rails'
+require 'sidekiq/extensions/action_mailer'
+require 'sidekiq/extensions/active_record'
+
+Sidekiq.hook_rails!
+
+class TestInline < MiniTest::Unit::TestCase
+  describe 'sidekiq inline testing' do
+    class InlineError < RuntimeError; end
+
+    class InlineWorker
+      include Sidekiq::Worker
+      def perform(pass)
+        raise InlineError unless pass
+      end
+    end
+
+    class InlineFooMailer < ActionMailer::Base
+      def bar(str)
+        raise InlineError
+      end
+    end
+
+    class InlineFooModel < ActiveRecord::Base
+      def self.bar(str)
+        raise InlineError
+      end
+    end
+
+    before do
+      load 'sidekiq/testing/inline.rb'
+    end
+
+    after do
+      Sidekiq::Worker::ClassMethods.class_eval do
+        remove_method :perform_async
+        alias_method :perform_async, :perform_async_old
+        remove_method :perform_async_old
+      end
+    end
+
+    it 'stubs the async call when in testing mode' do
+      assert InlineWorker.perform_async(true)
+
+      assert_raises InlineError do
+        InlineWorker.perform_async(false)
+      end
+    end
+
+    it 'stubs the delay call on mailers' do
+      assert_raises InlineError do
+        InlineFooMailer.delay.bar('three')
+      end
+    end
+
+    it 'stubs the delay call on models' do
+      assert_raises InlineError do
+        InlineFooModel.delay.bar('three')
+      end
+    end
+
+    it 'stubs the enqueue call when in testing mode' do
+      assert Sidekiq::Client.enqueue(InlineWorker, true)
+
+      assert_raises InlineError do
+        Sidekiq::Client.enqueue(InlineWorker, false)
+      end
+    end
+  end
+end
