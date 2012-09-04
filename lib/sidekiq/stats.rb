1e20288f33f739e1a575f4a10c98bf0190dc9f51
jc00ke <jesse@jc00ke.com>
2012-09-04 09:59:02 -0700

    Ditch pipelined calls for multi

--- lib/sidekiq/stats.rb
+++ lib/sidekiq/stats.rb
@@ -3,22 +3,21 @@ module Sidekiq
 
   def info
     results = {}
-    futures = {}
-    queues_with_sizes = Sidekiq.redis { |conn|
-      conn.pipelined do
-        futures[:processed] = conn.get('stat:processed')
-        futures[:failed] = conn.get('stat:failed')
-        futures[:queues] = conn.smembers('queues')
+    processed, failed, queues = Sidekiq.redis { |conn|
+      conn.multi do
+        conn.get('stat:processed')
+        conn.get('stat:failed')
+        conn.smembers('queues')
       end
     }
     results[:queues_with_sizes] = Sidekiq.redis do |conn|
-      futures[:queues].value.inject({}) { |memo, q|
+      queues.inject({}) { |memo, q|
         memo[q] = conn.llen("queue:#{q}")
         memo
       }.sort_by { |_, size| size }
     end
-    results[:processed] = (futures[:processed].value || 0).to_i
-    results[:failed] = (futures[:failed].value || 0).to_i
+    results[:processed] = (processed || 0).to_i
+    results[:failed] = (failed || 0).to_i
     results[:backlog] = results[:queues_with_sizes].
                           map {|_, size| size }.
                           inject(0) {|memo, val| memo + val }
@@ -29,7 +28,7 @@ module Sidekiq
     return info[:backlog] if queues.empty?
 
     Sidekiq.redis { |conn|
-      conn.pipelined {
+      conn.multi {
         queues.map { |q| conn.llen("queue:#{q}") }
       }
     }.inject(0) { |memo, count| memo += count }
