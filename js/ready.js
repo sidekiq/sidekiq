5039cbceb289d10176b19bde67b23dcd48ce8d6a
Mike Perham <mperham@gmail.com>
2012-10-01 19:42:02 -0700

    Move Sidekiq Pro site to sidekiq.org/pro

---
+++ js/ready.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  if (document.documentElement.clientWidth >= 768) {
+    var names = $('#features .names li');
+    var slider = $('#features .slider').bxSlider({
+      controls: false,
+      onBeforeSlide: function(slide) {
+        names.removeClass('selected');
+        $(names[slide]).addClass('selected');
+      }
+    });
+    names.click(function(e) {
+      var slide = names.index(this);
+      slider.goToSlide(slide);
+    });
+  }
+
+});
