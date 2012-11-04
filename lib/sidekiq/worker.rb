fb4850bdf101c072a040a38eaa95dccaa4b538fd
Mike Perham <mperham@gmail.com>
2012-11-03 22:05:37 -0700

    complex -> simple

--- lib/sidekiq/worker.rb
+++ lib/sidekiq/worker.rb
@@ -21,10 +21,11 @@ module Sidekiq
   #
   # Note that perform_async is a class method, perform is an instance method.
   module Worker
+    attr_accessor :jid
+
     def self.included(base)
       base.extend(ClassMethods)
       base.class_attribute :sidekiq_options_hash
-      base.send(:attr_accessor, :jid)
     end
 
     def logger
