ae473122ae4e56ecb933a5ecbe2bea6185be7795
Darío Javier Cravero <dario@qinnova.com.ar>
2013-03-08 00:08:09 -0300

    Padrino now loads every dependency (I18n, etc.), workers in app/workers and mounted apps! :) The mailer is also supported, you can now do Padrino.delay.deliver(:mailer, :mail, .....) and it just works! :D

--- lib/sidekiq/frameworks/padrino.rb
+++ lib/sidekiq/frameworks/padrino.rb
@@ -4,34 +4,18 @@ module Sidekiq
       ::ActiveRecord::Base.send(:include, Sidekiq::Extensions::ActiveRecord)
     end
 
+    # Do this for Padrino mailer?
     # if defined?(::ActionMailer)
     #   ::ActionMailer::Base.extend(Sidekiq::Extensions::ActionMailer)
     # end
-  end
 
-  # module Padrino
-  #   class << self              
-  #     ##
-  #     # Main class that register this extension.
-  #     #
-  #     def registered(app)      
-  #       app.send(:include, InstanceMethods)
-  #       app.extend(ClassMethods)
-  #       app.@_load_paths << File.expand_path("#{app.root}/app/workers") if File.exist?("#{config.root}/app/workers")
-  #     end
-  #     alias :included :registered     
-  #   end
-  # end
+    # Load Sidekiq workers if any
+    ::Padrino.require_dependencies Dir[::Padrino.root('app', 'workers', '**', '*.rb')] if File.exist?(::Padrino.root('app', 'workers'))
+
+    # Load mounted apps
+    ::Padrino.mounted_apps.each do |app|
+      puts "=> Loading Application #{app.app_class}"
+      app.app_obj.setup_application!
+    end
+  end
 end
-# TODO Implement. We need to load the workers and hook_padrino from this backend.
-# Once again, perhaps padrino.rb and rails.rb could be namespaced into frameworks?
-# module Padrino
-#   class Application
-#     register Sidekiq::Padrino
-#     # config.autoload_paths << File.expand_path("#{config.root}/app/workers") if File.exist?("#{config.root}/app/workers")
-# 
-#     initializer 'sidekiq' do
-#       Sidekiq.hook_padrino!
-#     end
-#   end
-# end
