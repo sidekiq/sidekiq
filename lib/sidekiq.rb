d297f1fefcafa8197b8c608d5470b90c26fc3811
Mike Perham <mperham@gmail.com>
2013-03-26 21:16:42 -0700

    Remove deprecated Sidekiq.size

--- lib/sidekiq.rb
+++ lib/sidekiq.rb
@@ -114,16 +114,4 @@ module Sidekiq
     self.options[:poll_interval] = interval
   end
 
-  ##
-  # deprecated
-  def self.size(*queues)
-    return Sidekiq::Stats.new.enqueued if queues.empty?
-
-    Sidekiq.redis { |conn|
-      conn.multi {
-        queues.map { |q| conn.llen("queue:#{q}") }
-      }
-    }.inject(0) { |memo, count| memo + count }
-  end
-
 end
