03d47b3f5cb24d6a41415e7e94102311e8e6d062
Jonathan Roes <jon@skookum.com>
2013-04-17 10:51:23 -0400

    use behavior syntax consistent with rest of test suite

--- test/test_fetch.rb
+++ test/test_fetch.rb
@@ -2,42 +2,43 @@ require 'helper'
 require 'sidekiq/fetch'
 
 class TestFetcher < MiniTest::Unit::TestCase
-
-  def setup
-    Sidekiq.redis = { :namespace => 'fuzzy' }
-    Sidekiq.redis do |conn|
-      conn.flushdb
-      conn.rpush('queue:basic', 'msg')
+  describe 'fetcher' do
+    before do
+      Sidekiq.redis = { :namespace => 'fuzzy' }
+      Sidekiq.redis do |conn|
+        conn.flushdb
+        conn.rpush('queue:basic', 'msg')
+      end
     end
-  end
 
-  def test_basic_fetch_retrieve
-    fetch = Sidekiq::BasicFetch.new(:queues => ['basic', 'bar'])
-    uow = fetch.retrieve_work
-    refute_nil uow
-    assert_equal 'basic', uow.queue_name
-    assert_equal 'msg', uow.message
-    q = Sidekiq::Queue.new('basic')
-    assert_equal 0, q.size
-    uow.requeue
-    assert_equal 1, q.size
-    assert_nil uow.acknowledge
-  end
+    it 'retrieves' do
+      fetch = Sidekiq::BasicFetch.new(:queues => ['basic', 'bar'])
+      uow = fetch.retrieve_work
+      refute_nil uow
+      assert_equal 'basic', uow.queue_name
+      assert_equal 'msg', uow.message
+      q = Sidekiq::Queue.new('basic')
+      assert_equal 0, q.size
+      uow.requeue
+      assert_equal 1, q.size
+      assert_nil uow.acknowledge
+    end
 
-  def test_basic_fetch_strict_retrieve
-    fetch = Sidekiq::BasicFetch.new(:queues => ['basic', 'bar', 'bar'], :strict => true)
-    cmd = fetch.queues_cmd
-    assert_equal cmd, ['queue:basic', 'queue:bar', 1]
-  end
+    it 'retrieves with strict setting' do
+      fetch = Sidekiq::BasicFetch.new(:queues => ['basic', 'bar', 'bar'], :strict => true)
+      cmd = fetch.queues_cmd
+      assert_equal cmd, ['queue:basic', 'queue:bar', 1]
+    end
 
-  def test_basic_fetch_bulk_requeue
-    q1 = Sidekiq::Queue.new('foo')
-    q2 = Sidekiq::Queue.new('bar')
-    assert_equal 0, q1.size
-    assert_equal 0, q2.size
-    uow = Sidekiq::BasicFetch::UnitOfWork
-    Sidekiq::BasicFetch.bulk_requeue([uow.new('fuzzy:queue:foo', 'bob'), uow.new('fuzzy:queue:foo', 'bar'), uow.new('fuzzy:queue:bar', 'widget')])
-    assert_equal 2, q1.size
-    assert_equal 1, q2.size
+    it 'bulk requeues' do
+      q1 = Sidekiq::Queue.new('foo')
+      q2 = Sidekiq::Queue.new('bar')
+      assert_equal 0, q1.size
+      assert_equal 0, q2.size
+      uow = Sidekiq::BasicFetch::UnitOfWork
+      Sidekiq::BasicFetch.bulk_requeue([uow.new('fuzzy:queue:foo', 'bob'), uow.new('fuzzy:queue:foo', 'bar'), uow.new('fuzzy:queue:bar', 'widget')])
+      assert_equal 2, q1.size
+      assert_equal 1, q2.size
+    end
   end
 end
