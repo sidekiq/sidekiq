981cb0d50a26785eeb1e1a438d3457732c2f4091
Kevin Sheurs <ksheurs@gmail.com>
2013-08-13 23:23:07 -0400

    find by id active_record extension re: #278

--- lib/sidekiq/extensions/generic_proxy.rb
+++ lib/sidekiq/extensions/generic_proxy.rb
@@ -15,7 +15,11 @@ module Sidekiq
         # serialize the objects to a String.  The YAML will be converted
         # to JSON and then deserialized on the other side back into a
         # Ruby object.
-        obj = [@target, name, args]
+        if @performable == DelayedModel
+          obj = [@target.class.name, @target.id, name, args]
+        else
+          obj = [@target, name, args]
+        end
         @performable.client_push({ 'class' => @performable, 'args' => [::YAML.dump(obj)] }.merge(@opts))
       end
     end
