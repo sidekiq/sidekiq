c7c8044d75a75502823ccc02ceb5ec7ee0572007
Mike Perham <mperham@gmail.com>
2013-09-12 14:14:17 -0700

    Minor refactoring of chain

--- lib/sidekiq/middleware/chain.rb
+++ lib/sidekiq/middleware/chain.rb
@@ -85,23 +85,23 @@ module Sidekiq
       def insert_before(oldklass, newklass, *args)
         i = entries.index { |entry| entry.klass == newklass }
         new_entry = i.nil? ? Entry.new(newklass, *args) : entries.delete_at(i)
-        i = entries.find_index { |entry| entry.klass == oldklass } || 0
+        i = entries.index { |entry| entry.klass == oldklass } || 0
         entries.insert(i, new_entry)
       end
 
       def insert_after(oldklass, newklass, *args)
         i = entries.index { |entry| entry.klass == newklass }
         new_entry = i.nil? ? Entry.new(newklass, *args) : entries.delete_at(i)
-        i = entries.find_index { |entry| entry.klass == oldklass } || entries.count - 1
+        i = entries.index { |entry| entry.klass == oldklass } || entries.count - 1
         entries.insert(i+1, new_entry)
       end
 
       def exists?(klass)
-        entries.any? { |entry| entry.klass == klass }
+        any? { |entry| entry.klass == klass }
       end
 
       def retrieve
-        entries.map(&:make_new)
+        map(&:make_new)
       end
 
       def clear
