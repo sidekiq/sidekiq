b271c3eb4dfa63fa36da49b2e439bddee1aa4eec
Péricles Dias <pericles.dias@gmail.com>
2013-10-03 22:07:22 +0200

    Force utf-8

--- lib/sidekiq/extensions/marshal_proxy.rb
+++ lib/sidekiq/extensions/marshal_proxy.rb
@@ -2,7 +2,7 @@ module Sidekiq
   module Extensions
     class MarshalProxy < Proxy
       def serialize(obj)
-        ::Marshal.dump(obj)
+        ::Marshal.dump(obj).force_encoding('ISO-8859-1').encode('UTF-8')
       end
     end
   end
