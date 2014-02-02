9dc4e065d5829b749e8e9351cc93c31a126087a4
Mike Perham <mperham@gmail.com>
2014-02-01 21:13:34 -0800

    Remove built-in error notification service support

--- lib/sidekiq/exception_handler.rb
+++ lib/sidekiq/exception_handler.rb
@@ -5,35 +5,7 @@ module Sidekiq
       Sidekiq.logger.warn(ctxHash) if !ctxHash.empty?
       Sidekiq.logger.warn ex
       Sidekiq.logger.warn ex.backtrace.join("\n") unless ex.backtrace.nil?
-      # This list of services is getting a bit ridiculous.
-      # For future error services, please add your own
-      # middleware like BugSnag does:
-      # https://github.com/bugsnag/bugsnag-ruby/blob/master/lib/bugsnag/sidekiq.rb
-      send_to_airbrake(ctxHash, ex) if defined?(::Airbrake)
-      send_to_honeybadger(ctxHash, ex) if defined?(::Honeybadger)
-      send_to_exceptional(ctxHash, ex) if defined?(::Exceptional)
-      send_to_exception_notifier(ctxHash, ex) if defined?(::ExceptionNotifier)
     end
 
-    private
-
-    def send_to_airbrake(hash, ex)
-      ::Airbrake.notify_or_ignore(ex, :parameters => hash)
-    end
-
-    def send_to_honeybadger(hash, ex)
-      ::Honeybadger.notify_or_ignore(ex, :parameters => hash)
-    end
-
-    def send_to_exceptional(hash, ex)
-      if ::Exceptional::Config.should_send_to_api?
-        ::Exceptional.context(hash)
-        ::Exceptional::Remote.error(::Exceptional::ExceptionData.new(ex))
-      end
-    end
-
-    def send_to_exception_notifier(hash, ex)
-      ::ExceptionNotifier.notify_exception(ex, :data => {:message => hash})
-    end
   end
 end
