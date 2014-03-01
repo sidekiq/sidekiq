fabb6fe6aeb8ea33b2557de3af3dd837c2a61b1d
Scott Klein <scott@scottkle.in>
2014-03-01 12:47:33 -0700

    logging controls on a per-worker basis

--- lib/sidekiq/middleware/server/logging.rb
+++ lib/sidekiq/middleware/server/logging.rb
@@ -7,11 +7,19 @@ module Sidekiq
           Sidekiq::Logging.with_context("#{worker.class.to_s} JID-#{item['jid']}") do
             begin
               start = Time.now
-              logger.info { "start" }
+              worker.logger.info { "start" }
               yield
-              logger.info { "done: #{elapsed(start)} sec" }
+
+              elapsed = elapsed(start)
+              
+              # if execution_threshold is set, let's respect it with WARN on too long of job
+              if worker.logger.execution_threshold && worker.logger.execution_threshold.to_f < elapsed
+                worker.logger.warn { "done: #{elapsed} sec" }
+              else
+                worker.logger.info { "done: #{elapsed} sec" }
+              end
             rescue Exception
-              logger.info { "fail: #{elapsed(start)} sec" }
+              worker.logger.warn { "fail: #{elapsed(start)} sec" }
               raise
             end
           end
@@ -19,10 +27,6 @@ module Sidekiq
 
         def elapsed(start)
           (Time.now - start).to_f.round(3)
-        end
-
-        def logger
-          Sidekiq.logger
         end
       end
     end
