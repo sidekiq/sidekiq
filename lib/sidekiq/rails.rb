e5687d7184f66d25d93e8fbbd1d87f31cb72b889
Devaroop <devaroop123@yahoo.co.in>
2014-04-24 19:41:05 +0530

    Removes the generic aliases which MAY clash with names of already declared by other applications

--- lib/sidekiq/rails.rb
+++ lib/sidekiq/rails.rb
@@ -9,6 +9,21 @@ module Sidekiq
     end
   end
 
+  # Removes the generic aliases which MAY clash with names of already
+  #  created methods by other applications. The methods `sidekiq_delay`,
+  #  `sidekiq_delay_for` and `sidekiq_delay_until` can be used instead.
+  def self.namespace_delay_methods
+    [Extensions::ActiveRecord, 
+     Extensions::ActionMailer, 
+     Extensions::Klass].each do |mod|
+      mod.module_eval do
+        remove_method :delay if respond_to?(:delay)
+        remove_method :delay_for if respond_to?(:delay_for)
+        remove_method :delay_until if respond_to?(:delay_until)
+      end
+    end
+  end
+
   class Rails < ::Rails::Engine
     config.autoload_paths << File.expand_path("#{config.root}/app/workers") if File.exist?("#{config.root}/app/workers")
 
