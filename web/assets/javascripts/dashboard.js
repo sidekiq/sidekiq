123d7bb2f427035b37887818102ef966ee52f740
Brandon Hilkert <brandonhilkert@gmail.com>
2014-05-21 16:37:49 -0400

    Update history graph with new hover legend

--- web/assets/javascripts/dashboard.js
+++ web/assets/javascripts/dashboard.js
@@ -139,10 +139,43 @@ var historyGraph = function() {
 
   graph.render();
 
-  var hoverDetail = new Rickshaw.Graph.HoverDetail({
-    graph: graph,
-    yFormatter: function(y) { return Math.floor(y).numberWithDelimiter() },
+  var legend = document.querySelector('#history-legend');
+  var Hover = Rickshaw.Class.create(Rickshaw.Graph.HoverDetail, {
+    render: function(args) {
+      legend.innerHTML = args.formattedXValue;
+
+      args.detail.sort(function(a, b) { return a.order - b.order }).forEach( function(d) {
+        var line = document.createElement('div');
+        line.className = 'line';
+
+        var swatch = document.createElement('div');
+        swatch.className = 'swatch';
+        swatch.style.backgroundColor = d.series.color;
+
+        var label = document.createElement('div');
+        label.className = 'tag';
+        label.innerHTML = d.name + ": " + Math.floor(d.formattedYValue);
+
+        line.appendChild(swatch);
+        line.appendChild(label);
+
+        legend.appendChild(line);
+
+        var dot = document.createElement('div');
+        dot.className = 'dot';
+        dot.style.top = graph.y(d.value.y0 + d.value.y) + 'px';
+        dot.style.borderColor = d.series.color;
+
+        this.element.appendChild(dot);
+
+        dot.className = 'dot active';
+
+        this.show();
+
+      }, this );
+    }
   });
+  var hover = new Hover( { graph: graph } );
 }
 
 var createSeries = function(obj) {
