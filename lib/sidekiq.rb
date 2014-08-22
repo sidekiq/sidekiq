59e353ce474dd253d5a2edabe3d004819f676666
Abdelkader Boudih <terminale@gmail.com>
2014-08-22 15:45:50 +0000

    [Feature] Remove engine in favor of railtie.

--- lib/sidekiq.rb
+++ lib/sidekiq.rb
@@ -154,4 +154,4 @@ end
 require 'sidekiq/extensions/class_methods'
 require 'sidekiq/extensions/action_mailer'
 require 'sidekiq/extensions/active_record'
-require 'sidekiq/rails' if defined?(::Rails::Engine)
+require 'sidekiq/railtie' if defined?(::Rails)
--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -55,5 +55,7 @@ module Myapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.sidekiq.app_url = '/'
   end
 end
