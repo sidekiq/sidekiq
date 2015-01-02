c749ef1b822089bf4591e8812b9b507582ce450f
Andrew Narkewicz <anark6@gmail.com>
2015-01-02 11:42:47 -0800

    Log backtraces at debug log level

--- lib/sidekiq/exception_handler.rb
+++ lib/sidekiq/exception_handler.rb
@@ -7,7 +7,7 @@ module Sidekiq
       def call(ex, ctxHash)
         Sidekiq.logger.warn(ctxHash) if !ctxHash.empty?
         Sidekiq.logger.warn ex
-        Sidekiq.logger.warn ex.backtrace.join("\n") unless ex.backtrace.nil?
+        Sidekiq.logger.debug ex.backtrace.join("\n") unless ex.backtrace.nil?
       end
 
       # Set up default handler which just logs the error
@@ -21,7 +21,7 @@ module Sidekiq
         rescue => ex
           Sidekiq.logger.error "!!! ERROR HANDLER THREW AN ERROR !!!"
           Sidekiq.logger.error ex
-          Sidekiq.logger.error ex.backtrace.join("\n") unless ex.backtrace.nil?
+          Sidekiq.logger.debug ex.backtrace.join("\n") unless ex.backtrace.nil?
         end
       end
     end
