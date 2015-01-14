8b1241e5175406daa2b400e07d2214962c6175c5
Mike Perham <mperham@gmail.com>
2015-01-14 12:00:29 -0800

    Remove warning for a long dead option

--- lib/sidekiq/worker.rb
+++ lib/sidekiq/worker.rb
@@ -65,7 +65,6 @@ module Sidekiq
       #   :pool - use the given Redis connection pool to push this type of job to a given shard.
       def sidekiq_options(opts={})
         self.sidekiq_options_hash = get_sidekiq_options.merge((opts || {}).stringify_keys)
-        ::Sidekiq.logger.warn("#{self.name} - :timeout is unsafe and support has been removed from Sidekiq, see http://bit.ly/OtYpK for details") if opts.include? :timeout
       end
 
       def sidekiq_retry_in(&block)
