cd7e160d67301f260b3fd7c471ab2a1a740108a9
Mike Perham <mperham@gmail.com>
2015-01-16 13:59:35 -0800

    Fix race condition in test

--- test/test_manager.rb
+++ test/test_manager.rb
@@ -121,7 +121,7 @@ class TestManager < Sidekiq::Test
           info = Sidekiq.redis { |c| c.hmget('identity', 'busy') }
           assert_equal ["1"], info
           expires = Sidekiq.redis { |c| c.pttl('identity') }
-          assert_in_delta 60000, expires, 50
+          assert_in_delta 60000, expires, 500
         end
       end
 
