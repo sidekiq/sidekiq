97363210b47a4f8a1d8c1233aaa059d6643f5040
Mike Perham <mperham@gmail.com>
2015-10-20 22:04:36 -0700

    For safety, flushdb Redis before each test

--- test/test_actors.rb
+++ test/test_actors.rb
@@ -14,116 +14,120 @@ class TestActors < Sidekiq::Test
     end
   end
 
-  describe 'scheduler' do
-    it 'can start and stop' do
-      f = Sidekiq::Scheduled::Poller.new
-      f.start
-      f.terminate
-    end
-
-    it 'can schedule' do
-      Sidekiq.redis {|c| c.flushdb}
-
-      ss = Sidekiq::ScheduledSet.new
-      q = Sidekiq::Queue.new
-
-      SomeWorker.perform_in(0.01, 0)
-
-      assert_equal 0, q.size
-      assert_equal 1, ss.size
-
-      sleep 0.015
-      s = Sidekiq::Scheduled::Poller.new
-      s.enqueue
-      assert_equal 1, q.size
-      assert_equal 0, ss.size
-      s.terminate
-    end
-  end
-
-  describe 'processor' do
+  describe 'threads' do
     before do
-      $count = 0
+      Sidekiq.redis {|c| c.flushdb}
     end
 
-    it 'can start and stop' do
-      f = Sidekiq::Processor.new(Mgr.new)
-      f.terminate
-    end
-
-    class Mgr
-      attr_reader :latest_error
-      attr_reader :mutex
-      attr_reader :cond
-      def initialize
-        @mutex = ::Mutex.new
-        @cond = ::ConditionVariable.new
+    describe 'scheduler' do
+      it 'can start and stop' do
+        f = Sidekiq::Scheduled::Poller.new
+        f.start
+        f.terminate
       end
-      def processor_died(inst, err)
-        @latest_error = err
-        @mutex.synchronize do
-          @cond.signal
+
+      it 'can schedule' do
+        ss = Sidekiq::ScheduledSet.new
+        q = Sidekiq::Queue.new
+
+        SomeWorker.perform_in(0.01, 0)
+
+        assert_equal 0, q.size
+        assert_equal 1, ss.size
+
+        sleep 0.015
+        s = Sidekiq::Scheduled::Poller.new
+        s.enqueue
+        assert_equal 1, q.size
+        assert_equal 0, ss.size
+        s.terminate
+      end
+    end
+
+    describe 'processor' do
+      before do
+        $count = 0
+      end
+
+      it 'can start and stop' do
+        f = Sidekiq::Processor.new(Mgr.new)
+        f.terminate
+      end
+
+      class Mgr
+        attr_reader :latest_error
+        attr_reader :mutex
+        attr_reader :cond
+        def initialize
+          @mutex = ::Mutex.new
+          @cond = ::ConditionVariable.new
+        end
+        def processor_died(inst, err)
+          @latest_error = err
+          @mutex.synchronize do
+            @cond.signal
+          end
+        end
+        def processor_stopped(inst)
+          @mutex.synchronize do
+            @cond.signal
+          end
+        end
+        def options
+          { :concurrency => 3, :queues => ['default'] }
         end
       end
-      def processor_stopped(inst)
-        @mutex.synchronize do
-          @cond.signal
+
+      it 'can process' do
+        mgr = Mgr.new
+
+        p = Sidekiq::Processor.new(mgr)
+        SomeWorker.perform_async(0)
+
+        a = $count
+        p.process_one
+        b = $count
+        assert_equal a + 1, b
+      end
+
+      it 'deals with errors' do
+        mgr = Mgr.new
+
+        p = Sidekiq::Processor.new(mgr)
+        SomeWorker.perform_async("boom")
+
+        a = $count
+        mgr.mutex.synchronize do
+          p.start
+          mgr.cond.wait(mgr.mutex)
         end
+        b = $count
+        assert_equal a, b
+
+        sleep 0.001
+        assert_equal false, p.thread.status
+        p.terminate(true)
+        refute_nil mgr.latest_error
+        assert_equal RuntimeError, mgr.latest_error.class
       end
-      def options
-        { :concurrency => 3, :queues => ['default'] }
-      end
-    end
 
-    it 'can process' do
-      mgr = Mgr.new
+      it 'gracefully kills' do
+        mgr = Mgr.new
 
-      p = Sidekiq::Processor.new(mgr)
-      SomeWorker.perform_async(0)
+        p = Sidekiq::Processor.new(mgr)
+        SomeWorker.perform_async(1)
 
-      a = $count
-      p.process_one
-      b = $count
-      assert_equal a + 1, b
-    end
-
-    it 'deals with errors' do
-      mgr = Mgr.new
-
-      p = Sidekiq::Processor.new(mgr)
-      SomeWorker.perform_async("boom")
-
-      a = $count
-      mgr.mutex.synchronize do
+        a = $count
         p.start
-        mgr.cond.wait(mgr.mutex)
+        sleep(0.02)
+        p.terminate
+        p.kill(true)
+
+        b = $count
+        assert_equal a, b
+        assert_equal false, p.thread.status
+        refute mgr.latest_error, mgr.latest_error.to_s
       end
-      b = $count
-      assert_equal a, b
-
-      sleep 0.001
-      assert_equal false, p.thread.status
-      p.terminate(true)
-      refute_nil mgr.latest_error
-      assert_equal RuntimeError, mgr.latest_error.class
-    end
-
-    it 'gracefully kills' do
-      mgr = Mgr.new
-
-      p = Sidekiq::Processor.new(mgr)
-      SomeWorker.perform_async(1)
-
-      a = $count
-      p.start
-      sleep(0.02)
-      p.terminate
-      p.kill(true)
-
-      b = $count
-      assert_equal a, b
-      assert_equal false, p.thread.status
-      refute mgr.latest_error, mgr.latest_error.to_s
     end
   end
 end
