3cc996aaeffb89702ce3bb1e3fd8f35a3e7fa17f
Brandon Hilkert <brandonhilkert@gmail.com>
2015-11-17 12:44:40 -0500

    Revert "Merge pull request #2661 from mhenrixon/master"
    This reverts commit fdb7ded6d81aaab76dfefcb337436eeb9c8c6211, reversing
    changes made to df739ca968581be2b6a998d5a210fcd720d91881.

--- lib/sidekiq/testing.rb
+++ lib/sidekiq/testing.rb
@@ -130,7 +130,7 @@ module Sidekiq
     #
     class << self
       def [](queue)
-        jobs[queue.to_s]
+        jobs[queue]
       end
 
       def jobs
@@ -203,7 +203,7 @@ module Sidekiq
 
       # Queue for this worker
       def queue
-        self.sidekiq_options["queue"].to_s
+        self.sidekiq_options["queue"]
       end
 
       # Jobs queued for this worker
