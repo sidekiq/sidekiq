2ad609b4b251412fb7779ca487beb0fbe7da72ff
Mike Perham <mperham@gmail.com>
2015-12-09 11:44:47 -0800

    Display created_at, fixes #2705

--- lib/sidekiq/web_helpers.rb
+++ lib/sidekiq/web_helpers.rb
@@ -193,6 +193,7 @@ module Sidekiq
       queue class args retry_count retried_at failed_at
       jid error_message error_class backtrace
       error_backtrace enqueued_at retry wrapped
+      created_at
     ))
 
     def retry_extra_items(retry_job)
