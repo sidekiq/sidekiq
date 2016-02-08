d547c2a8fc66ff3f973d69336ad15a4ebc13d159
Mike Perham <mperham@gmail.com>
2016-02-08 13:43:29 -0800

    aj testing

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -22,5 +22,6 @@ module Myapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.active_job.queue_adapter = :sidekiq
   end
 end
