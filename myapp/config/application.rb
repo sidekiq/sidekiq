3b538c66e98732315b2a440b5a8dabca906623ab
Mike Perham <mperham@gmail.com>
2016-10-14 10:22:11 -0700

    this isnt supported anymore

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -21,7 +21,6 @@ module Myapp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :sidekiq
   end
 end
