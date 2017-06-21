66d6ee35355186e31997282e926696229c522af9
Mike Perham <mperham@gmail.com>
2017-06-21 13:21:11 -0700

    timing is everything

--- test/test_testing_fake.rb
+++ test/test_testing_fake.rb
@@ -41,14 +41,14 @@ class TestFake < Sidekiq::Test
     it 'stubs the async call' do
       assert_equal 0, DirectWorker.jobs.size
       assert DirectWorker.perform_async(1, 2)
-      assert_in_delta Time.now.to_f, DirectWorker.jobs.last['enqueued_at'], 0.01
+      assert_in_delta Time.now.to_f, DirectWorker.jobs.last['enqueued_at'], 0.1
       assert_equal 1, DirectWorker.jobs.size
       assert DirectWorker.perform_in(10, 1, 2)
       refute DirectWorker.jobs.last['enqueued_at']
       assert_equal 2, DirectWorker.jobs.size
       assert DirectWorker.perform_at(10, 1, 2)
       assert_equal 3, DirectWorker.jobs.size
-      assert_in_delta 10.seconds.from_now.to_f, DirectWorker.jobs.last['at'], 0.01
+      assert_in_delta 10.seconds.from_now.to_f, DirectWorker.jobs.last['at'], 0.1
     end
 
     describe 'delayed' do
