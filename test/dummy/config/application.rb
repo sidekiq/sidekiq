731b5d69e400a6a0013f257414aaf3b50292d686
Mike Perham <mperham@gmail.com>
2019-02-08 08:53:50 -0800

    cleanup

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,5 +7,6 @@ module Dummy
     config.root = File.expand_path("../..", __FILE__)
     config.eager_load = false
     config.logger = Logger.new('/dev/null')
+    config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
