339812b379dd249b6f21af390da811b64a7b3c6a
Gearnode <bryan@frimin.fr>
2019-03-02 19:15:11 +0100

    Apply lint for  lib/sidekiq/paginator

--- lib/sidekiq/paginator.rb
+++ lib/sidekiq/paginator.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
+
 module Sidekiq
   module Paginator
-
-    def page(key, pageidx=1, page_size=25, opts=nil)
+    def page(key, pageidx = 1, page_size = 25, opts = nil)
       current_page = pageidx.to_i < 1 ? 1 : pageidx.to_i
       pageidx = current_page - 1
       total_size = 0
@@ -14,30 +14,29 @@ module Sidekiq
         type = conn.type(key)
 
         case type
-        when 'zset'
+        when "zset"
           rev = opts && opts[:reverse]
-          total_size, items = conn.multi do
+          total_size, items = conn.multi {
             conn.zcard(key)
             if rev
-              conn.zrevrange(key, starting, ending, :with_scores => true)
+              conn.zrevrange(key, starting, ending, with_scores: true)
             else
-              conn.zrange(key, starting, ending, :with_scores => true)
+              conn.zrange(key, starting, ending, with_scores: true)
             end
-          end
+          }
           [current_page, total_size, items]
-        when 'list'
-          total_size, items = conn.multi do
+        when "list"
+          total_size, items = conn.multi {
             conn.llen(key)
             conn.lrange(key, starting, ending)
-          end
+          }
           [current_page, total_size, items]
-        when 'none'
+        when "none"
           [1, 0, []]
         else
           raise "can't page a #{type}"
         end
       end
     end
-
   end
 end
