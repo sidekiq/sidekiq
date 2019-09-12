9c4241e7cc80abeb03661ed40a5acc31041ebf53
Mike Perham <mperham@gmail.com>
2019-09-12 10:15:08 -0700

    Assume 6.0

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -7,5 +7,6 @@ module Dummy
     config.root = File.expand_path("../..", __FILE__)
     config.eager_load = false
     config.logger = Logger.new('/dev/null')
+    config.load_defaults "6.0"
   end
 end
