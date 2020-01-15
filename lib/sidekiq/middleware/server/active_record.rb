ec3e138710095aee7b78fdf15602c76786f5cd29
Thomas Koppensteiner <ThomasKoppensteiner@users.noreply.github.com>
2020-01-15 17:53:32 +0100

    Fix Rails::VERSION check for sinatra and ActiveRecord (#4433)
    4432

--- lib/sidekiq/middleware/server/active_record.rb
+++ lib/sidekiq/middleware/server/active_record.rb
@@ -7,7 +7,7 @@ module Sidekiq
         def initialize
           # With Rails 5+ we must use the Reloader **always**.
           # The reloader handles code loading and db connection management.
-          if defined?(::Rails) && ::Rails::VERSION::MAJOR >= 5
+          if defined?(::Rails) && defined?(::Rails::VERSION) && ::Rails::VERSION::MAJOR >= 5
             raise ArgumentError, "Rails 5 no longer needs or uses the ActiveRecord middleware."
           end
         end
