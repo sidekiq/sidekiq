1207d2b5c14679bab55d1e7f408470ed94e7e6f7
Mike Perham <mperham@gmail.com>
2020-03-17 10:12:59 -0700

    Remove annoying bits of Rails that just make our life harder

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -1,6 +1,19 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
