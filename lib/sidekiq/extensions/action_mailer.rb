bc0e2fb414cea54cb9e329bde10fab774985377c
Akansha Kumari <39851943+akanshak984@users.noreply.github.com>
2020-08-25 19:55:47 +0530

    document update using YARD annotation (#4676)

--- lib/sidekiq/extensions/action_mailer.rb
+++ lib/sidekiq/extensions/action_mailer.rb
@@ -5,9 +5,10 @@ require "sidekiq/extensions/generic_proxy"
 module Sidekiq
   module Extensions
     ##
-    # Adds 'delay', 'delay_for' and `delay_until` methods to ActionMailer to offload arbitrary email
-    # delivery to Sidekiq.  Example:
+    # Adds +delay+, +delay_for+ and +delay_until+ methods to ActionMailer to offload arbitrary email
+    # delivery to Sidekiq.
     #
+    # @example
     #    UserMailer.delay.send_welcome_email(new_user)
     #    UserMailer.delay_for(5.days).send_welcome_email(new_user)
     #    UserMailer.delay_until(5.days.from_now).send_welcome_email(new_user)
