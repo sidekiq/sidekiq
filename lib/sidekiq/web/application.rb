767217133e64de9d07f1cf9595eb06a8e5f57c0b
Mike Perham <mperham@gmail.com>
2021-10-27 16:21:15 -0700

    Refactor Web UI javascripts
    - Remove jQuery
    - Remove dropdown.js, unused
    - Update app JS to use vanilla JS

--- lib/sidekiq/web/application.rb
+++ lib/sidekiq/web/application.rb
@@ -91,8 +91,8 @@ module Sidekiq
 
       @count = (params["count"] || 25).to_i
       @queue = Sidekiq::Queue.new(@name)
-      (@current_page, @total_size, @messages) = page("queue:#{@name}", params["page"], @count, reverse: params["direction"] == "asc")
-      @messages = @messages.map { |msg| Sidekiq::JobRecord.new(msg, @name) }
+      (@current_page, @total_size, @jobs) = page("queue:#{@name}", params["page"], @count, reverse: params["direction"] == "asc")
+      @jobs = @jobs.map { |msg| Sidekiq::JobRecord.new(msg, @name) }
 
       erb(:queue)
     end
