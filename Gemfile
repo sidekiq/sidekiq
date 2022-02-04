556bb6e0d678d3a3e758e7a0a2378c2965dc661f
Mike Perham <mperham@gmail.com>
2022-02-04 14:13:49 -0800

    Revert to Rails 6.x, need to support Ruby 2.6

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gemspec
 
 gem "rake"
 gem "redis-namespace"
-gem "rails", ">= 6.0.2"
+gem "rails", "~> 6.0"
 gem "sqlite3", platforms: :ruby
 gem "activerecord-jdbcsqlite3-adapter", platforms: :jruby
 
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -14,6 +14,6 @@ module Dummy
     config.root = File.expand_path("../..", __FILE__)
     config.eager_load = false
     config.logger = Logger.new('/dev/null')
-    config.load_defaults "7.0"
+    config.load_defaults "6.1"
   end
 end
