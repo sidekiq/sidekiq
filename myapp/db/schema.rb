5371e1fe21134bbe12985fb097c4b4880982abfb
Ben Ratelade <ratelade.benjamin@gmail.com>
2022-04-05 08:16:09 +1000

    undoig the changes to myapp/db/schema.rb

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2012_01_23_214055) do
+ActiveRecord::Schema.define(version: 2012_01_23_214055) do
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
