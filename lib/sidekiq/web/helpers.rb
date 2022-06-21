94ad7a5e9118dc1aa2fa87c97be4b6547cdc679d
Kevin Bongart <154600+KevinBongart@users.noreply.github.com>
2022-06-21 11:42:12 +0200

    Fix natural sort algorithm and add more test examples

--- lib/sidekiq/web/helpers.rb
+++ lib/sidekiq/web/helpers.rb
@@ -150,12 +150,22 @@ module Sidekiq
 
     # Sorts processes by hostname following the natural sort order so that
     # 'worker.1' < 'worker.2' < 'worker.10' < 'worker.20'
+    # '2.1.1.1' < '192.168.0.2' < '192.168.0.10'
     def sorted_processes
-      @sorted_processes ||= processes.to_a.sort_by do |process|
-        words = process['hostname'].split('.')
-        padding = words.map(&:size).max
+      @sorted_processes ||= begin
+        split_characters = /[\._-]/
 
-        words.map { |word| word.rjust(padding, '0') }
+        padding = processes.flat_map { |p| p['hostname'].split(split_characters) }.map(&:size).max
+
+        processes.to_a.sort_by do |process|
+          process['hostname'].split(split_characters).map do |substring|
+            # Left-pad the substring with '0' if it starts with a number or 'a'
+            # otherwise, so that '25' < 192' < 'a' ('025' < '192' < 'aaa')
+            padding_char = substring.chars.first.match?(/\d/) ? '0' : 'a'
+
+            substring.rjust(padding, padding_char)
+          end
+        end
       end
     end
 
