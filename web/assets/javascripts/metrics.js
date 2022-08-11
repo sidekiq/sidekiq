da81f58146fe4d58abe2fa25c47db9c68550fcdc
Adam McCrea <adam@adamlogic.com>
2022-08-11 12:17:55 -0400

    Only include top 5 in chart, change swatch element to checkbox

--- web/assets/javascripts/metrics.js
+++ web/assets/javascripts/metrics.js
@@ -2,18 +2,22 @@ function init() {
   const ctx = document.getElementById("metrics-chart");
   const colors = JSON.parse(ctx.getAttribute("data-colors"));
   const labels = JSON.parse(ctx.getAttribute("data-labels"));
-  const datasets = Object.entries(
-    JSON.parse(ctx.getAttribute("data-series"))
-  ).map(([k, v], i) => {
-    return {
-      label: k,
-      data: v,
-      borderColor: colors[i],
-      backgroundColor: colors[i],
-      borderWidth: 2,
-      pointRadius: 2,
-    };
-  });
+  const visible = JSON.parse(ctx.getAttribute("data-visible"));
+  const series = JSON.parse(ctx.getAttribute("data-series"));
+  const datasets = Object.entries(series)
+    .filter(([jobClass, data]) => {
+      return visible.includes(jobClass);
+    })
+    .map(([jobClass, data], i) => {
+      return {
+        label: jobClass,
+        data: data,
+        borderColor: colors[i],
+        backgroundColor: colors[i],
+        borderWidth: 2,
+        pointRadius: 2,
+      };
+    });
   const options = {
     aspectRatio: 4,
     scales: {
