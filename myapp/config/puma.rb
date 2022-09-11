4e2814001f7effda9bd9ac09f73055b79c9e77de
Mike Perham <mperham@gmail.com>
2022-09-11 10:44:16 -0700

    Need to preload

--- myapp/config/puma.rb
+++ myapp/config/puma.rb
@@ -37,7 +37,8 @@ workers ENV.fetch("WEB_CONCURRENCY", 2)
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+require "sidekiq"
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
