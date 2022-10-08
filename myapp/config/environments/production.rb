dda748849273c1375d74698711adad7e0dec1933
Abdelkader Boudih <terminale@gmail.com>
2022-10-08 23:50:43 +0100

    remove extra gems from the stack

--- myapp/config/environments/production.rb
+++ myapp/config/environments/production.rb
@@ -24,12 +24,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
