cf60006c3acbe9d4fa7524920273bf509c28604b
Jonathan del Strother <jdelStrother@gmail.com>
2022-11-14 12:19:18 +0000

    Allow shift-clicking to select ranges of jobs

--- web/assets/javascripts/application.js
+++ web/assets/javascripts/application.js
@@ -31,6 +31,7 @@ function addListeners() {
     node.addEventListener("click", addDataToggleListeners)
   })
 
+  addShiftClickListeners()
   updateFuzzyTimes();
   setLivePollFromUrl();
 
@@ -69,6 +70,23 @@ function addDataToggleListeners(event) {
   } else {
     full.style.display = 'block';
   }
+}
+
+function addShiftClickListeners() {
+  let checkboxes = Array.from(document.querySelectorAll(".shift_clickable"));
+  let lastChecked = null;
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("click", (e) => {
+      if (e.shiftKey && lastChecked) {
+        let myIndex = checkboxes.indexOf(checkbox);
+        let lastIndex = checkboxes.indexOf(lastChecked);
+        let [min, max] = [myIndex, lastIndex].sort();
+        let newState = checkbox.checked;
+        checkboxes.slice(min, max).forEach(c => c.checked = newState);
+      }
+      lastChecked = checkbox;
+    });
+  });
 }
 
 function updateFuzzyTimes() {
