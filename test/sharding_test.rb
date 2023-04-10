1019bc5bd702b8ff74ed3556ab5809e4fe788209
Mike Perham <mperham@gmail.com>
2023-04-10 12:52:52 -0700

    Add test to verify client middleware redis_pool when sharding

--- test/sharding_test.rb
+++ test/sharding_test.rb
@@ -8,6 +8,17 @@ class ShardJob
   include Sidekiq::Job
 end
 
+class ShardMiddleware
+  include Sidekiq::ClientMiddleware
+  def call(wrkr, job, q, pool)
+    # set a flag so we can inspect which shard is active
+    redis { |c| c.set("flag", job["jid"]) }
+    fl = pool.with { |c| c.get("flag") }
+    raise "Pool and redis are out of sync!" if fl != job["jid"]
+    yield
+  end
+end
+
 describe "Sharding" do
   before do
     @config = reset!
@@ -20,7 +31,31 @@ describe "Sharding" do
     @sh2.shutdown(&:close)
   end
 
+  def flags
+    [Sidekiq.redis_pool, @sh1, @sh2].map do |pool|
+      pool.with { |c| c.get("flag") }
+    end
+  end
+
   describe "client" do
+    it "redirects the middleware pool to the current shard" do
+      @config.client_middleware.add ShardMiddleware
+
+      jid1 = nil
+      Sidekiq::Client.via(@sh1) do
+        jid1 = ShardJob.perform_async
+        assert_equal [jid1, jid1, nil], flags
+      end
+      assert_equal [nil, jid1, nil], flags
+
+      jid2 = nil
+      Sidekiq::Client.via(@sh2) do
+        jid2 = ShardJob.perform_async
+        assert_equal [jid2, jid1, jid2], flags
+      end
+      assert_equal [nil, jid1, jid2], flags
+    end
+
     it "routes jobs to the proper shard" do
       q = Sidekiq::Queue.new
       ss = Sidekiq::ScheduledSet.new
