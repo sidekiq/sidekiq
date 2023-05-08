f0bcccc0494cc7f7bff2eeb0f189e464393e4c2f
Ricardo Amorim <mksm@iddqd.su>
2023-05-08 14:19:10 -0300

    Use LINDEX for consistent performance when calc queue latency (#5910)
    - LRANGE's complexity is O(S+N) where S is the distance of start offset
      from HEAD in Redis versions 4.x to 6.x and nearest HEAD or TAIL only
      in versions >= 7.0.
    - LINDEX list -1 returns the same as LRANGE list -1 -1 and has
      consistent performance across all Redis versions.

--- lib/sidekiq/api.rb
+++ lib/sidekiq/api.rb
@@ -92,11 +92,11 @@ module Sidekiq
           pipeline.zcard("retry")
           pipeline.zcard("dead")
           pipeline.scard("processes")
-          pipeline.lrange("queue:default", -1, -1)
+          pipeline.lindex("queue:default", -1)
         end
       }
 
-      default_queue_latency = if (entry = pipe1_res[6].first)
+      default_queue_latency = if (entry = pipe1_res[6])
         job = begin
           Sidekiq.load_json(entry)
         rescue
@@ -264,8 +264,8 @@ module Sidekiq
     # @return [Float] in seconds
     def latency
       entry = Sidekiq.redis { |conn|
-        conn.lrange(@rname, -1, -1)
-      }.first
+        conn.lindex(@rname, -1)
+      }
       return 0 unless entry
       job = Sidekiq.load_json(entry)
       now = Time.now.to_f
