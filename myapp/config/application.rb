090971913efbbe5d2a40671538531a0bee74be9d
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2023-11-13 12:54:58 +0900

    Fix broken CI by Standard Style Check
    This fixes some styles by `rake standard:fix_unsafely`.
    
    Ref: https://github.com/sidekiq/sidekiq/actions/runs/6817192931/job/18540155962#step:6:14

--- myapp/config/application.rb
+++ myapp/config/application.rb
@@ -21,7 +21,7 @@ module Myapp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
