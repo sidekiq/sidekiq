39a3d888e09404131edba61deeb9c4900d20398f
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-01-15 17:14:42 +0100

    remove service name from settings

--- test/helper.rb
+++ test/helper.rb
@@ -28,7 +28,6 @@ end
 
 # Configure default Minitest integration
 Datadog.configure do |c|
-  c.service = "sidekiq"
   c.ci.enabled = true
   c.ci.experimental_test_suite_level_visibility_enabled = true
   c.ci.instrument :minitest
