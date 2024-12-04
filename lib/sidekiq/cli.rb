fe22ff753099461f4a1766ad2bc4c14c12232b19
Mike Perham <mperham@gmail.com>
2024-12-04 11:34:46 -0800

    special case dragonfly's redis_version for now

--- lib/sidekiq/cli.rb
+++ lib/sidekiq/cli.rb
@@ -74,7 +74,9 @@ module Sidekiq # :nodoc:
       # fire startup and start multithreading.
       info = @config.redis_info
       ver = Gem::Version.new(info["redis_version"])
-      raise "You are connected to Redis #{ver}, Sidekiq requires Redis 7.2.0 or greater" if ver < Gem::Version.new("7.2.0")
+      if ver != "6.2.11" # TODO dragonfly
+        raise "You are connected to Redis #{ver}, Sidekiq requires Redis 7.2.0 or greater" if ver < Gem::Version.new("7.2.0")
+      end
 
       maxmemory_policy = info["maxmemory_policy"]
       if maxmemory_policy != "noeviction" && maxmemory_policy != ""
