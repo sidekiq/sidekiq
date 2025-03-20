d9dc71980b9ee923000830f2973e6b3719c057d3
Dimitris Vichos <dimitris@unhooked.gr>
2025-03-20 19:42:43 +0200

    fix: Redis version requirement in embedded mode (#6654)

--- lib/sidekiq/embedded.rb
+++ lib/sidekiq/embedded.rb
@@ -43,7 +43,7 @@ module Sidekiq
       # fire startup and start multithreading.
       info = config.redis_info
       ver = Gem::Version.new(info["redis_version"])
-      raise "You are connecting to Redis #{ver}, Sidekiq requires Redis 6.2.0 or greater" if ver < Gem::Version.new("6.2.0")
+      raise "You are connected to Redis #{ver}, Sidekiq requires Redis 7.0.0 or greater" if ver < Gem::Version.new("7.0.0")
 
       maxmemory_policy = info["maxmemory_policy"]
       if maxmemory_policy != "noeviction"
