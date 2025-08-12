e3a1f623776e0b7d655c11030027c7aa624a025f
Joe Van Dyk <joevandyk@crowdcow.com>
2025-08-12 05:19:59 -0700

    updates

--- lib/sidekiq/job/iterable.rb
+++ lib/sidekiq/job/iterable.rb
@@ -275,12 +275,10 @@ module Sidekiq
 
       def flush_state
         key = iteration_key
-        now = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
-        current_runtime = @_start_time ? (@_runtime + (now - @_start_time)) : @_runtime
         state = {
           "ex" => @_executions,
           "c" => Sidekiq.dump_json(@_cursor),
-          "rt" => current_runtime
+          "rt" => @_runtime
         }
 
         Sidekiq.redis do |conn|
