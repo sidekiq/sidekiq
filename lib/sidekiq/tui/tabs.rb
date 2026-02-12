3b66c250a7f18c5e1030ed2e9c5801179bbcfffe
Felipe Vogel <fps.vogel@gmail.com>
2026-02-11 21:42:04 -0500

    Move logic related to current/all tabs into Tabs

--- lib/sidekiq/tui/tabs.rb
+++ lib/sidekiq/tui/tabs.rb
@@ -1,7 +1,33 @@
 module Sidekiq
   class TUI
     module Tabs
-      def self.all = BaseTab.subclasses.sort_by(&:order)
+      def self.all
+        @all ||= BaseTab.subclasses.sort_by(&:order)
+      end
+
+      def self.current
+        @current ||= Tabs::Home
+      end
+
+      # Navigate tabs to the left or right.
+      # @param direction [Symbol] :left or :right
+      def self.navigate(direction)
+        index_change = (direction == :right) ? 1 : -1
+        @current = @all[(@all.index(@current) + index_change) % @all.size]
+        @current.reset_data
+      end
+
+      def self.showing
+        @showing ||= :main
+      end
+
+      def self.show_main
+        @showing = :main
+      end
+
+      def self.show_help
+        @showing = :help
+      end
     end
   end
 end
